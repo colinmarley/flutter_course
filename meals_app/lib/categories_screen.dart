b961e7536a833e1403e6d7547922263c7d8a7750
colinmarley <colinmarley94@gmail.com>
2019-08-30 16:05:08 -0700

    Lesson 158 Complete

--- meals_app/lib/categories_screen.dart
+++ meals_app/lib/categories_screen.dart
@@ -4,14 +4,14 @@ import './dummy_data.dart';
 import './category_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('DeliMeal'),),
+      appBar: AppBar(
+        title: const Text('DeliMeal'),
+      ),
       body: GridView(
-        padding: EdgeInsets.all(25),
+        padding: const EdgeInsets.all(25),
         children: DUMMY_CATEGORIES
             .map(
               (catData) => CategoryItem(catData.title, catData.color),
@@ -22,7 +22,8 @@ class CategoriesScreen extends StatelessWidget {
           childAspectRatio: 3 / 2,
           crossAxisSpacing: 20,
           mainAxisSpacing: 20,
-        )),
+        ),
+      ),
     );
   }
 }
